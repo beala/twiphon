c8fade5d0eed3bfd39fcfa8906c822807aa7b341
Alexander Beal <abeal@twitter.com>
2016-03-15 22:18:00 -0600

    Stylish haskell.

--- app/Main.hs
+++ app/Main.hs
@@ -2,25 +2,33 @@
 
 module Main where
 
-import Network.HTTP.Client.TLS (tlsManagerSettings)
-import Network.HTTP.Client (newManager, Request, parseUrl, setQueryString, httpLbs, responseBody, Manager)
-import Web.Authenticate.OAuth (signOAuth, def, oauthConsumerSecret, oauthConsumerKey, Credential(..), oauthServerName, OAuth)
-import qualified Data.ByteString as BS
+import           Control.Exception.Base     (Exception)
+import           Control.Monad              (forever)
+import           Control.Monad.Catch        (MonadThrow, throwM)
+import           Control.Monad.IO.Class
+import           Data.Aeson                 (decode, encode)
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Char8      as BS8
 import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.ByteString.Char8 as BS8
-import Control.Monad.Catch (MonadThrow, throwM)
-import Data.Monoid ((<>))
-import Web.Twitter.Types (Status(..))
-import Data.Aeson (decode, encode)
-import Control.Monad.IO.Class
-import Pipes
-import qualified Pipes.Prelude as P
-import Data.Foldable (traverse_)
-import Control.Exception.Base (Exception)
-import Safe (lastMay)
-import Data.Maybe (catMaybes)
-import Control.Monad (forever)
-import Options.Applicative (Parser, long, help, option, auto, optional, info, fullDesc, progDesc, ParserInfo, execParser, helper, strOption)
+import           Data.Foldable              (traverse_)
+import           Data.Maybe                 (catMaybes)
+import           Data.Monoid                ((<>))
+import           Network.HTTP.Client        (Manager, Request, httpLbs,
+                                             newManager, parseUrl, responseBody,
+                                             setQueryString)
+import           Network.HTTP.Client.TLS    (tlsManagerSettings)
+import           Options.Applicative        (Parser, ParserInfo, auto,
+                                             execParser, fullDesc, help, helper,
+                                             info, long, option, optional,
+                                             progDesc, strOption)
+import           Pipes
+import qualified Pipes.Prelude              as P
+import           Safe                       (lastMay)
+import           Web.Authenticate.OAuth     (Credential (..), OAuth, def,
+                                             oauthConsumerKey,
+                                             oauthConsumerSecret,
+                                             oauthServerName, signOAuth)
+import           Web.Twitter.Types          (Status (..))
 
 main :: IO ()
 main = do
@@ -31,13 +39,13 @@ main = do
   let maxId = BS8.pack <$> (configMaxId config)
   let screenName = BS8.pack $ configScreenName config
   manager <- newManager tlsManagerSettings
-  runEffect $ getUserTimeline' screenName maxId oauth credential manager >-> 
-              limitPipe >-> 
-              statusToJson >-> 
+  runEffect $ getUserTimeline' screenName maxId oauth credential manager >->
+              limitPipe >->
+              statusToJson >->
               printByteString
 
 twitterOAuth :: Config -> OAuth
-twitterOAuth config = def 
+twitterOAuth config = def
   { oauthServerName = "Twitter"
   , oauthConsumerKey = BS8.pack $ configConsumerKey config
   , oauthConsumerSecret = BS8.pack $ configConsumerSecret config
@@ -48,7 +56,7 @@ twitterCredential config = Credential [(BS8.pack $ configToken config, BS8.pack 
 
 userTimelineReq :: MonadThrow m => BS.ByteString -> Maybe BS.ByteString -> m Request
 userTimelineReq screenName maxId = do
-  initReq <- parseUrl "https://api.twitter.com/1.1/statuses/user_timeline.json" 
+  initReq <- parseUrl "https://api.twitter.com/1.1/statuses/user_timeline.json"
   let params = catMaybes [ Just ("screen_name", Just screenName)
                          , fmap (\x -> ("max_id", Just x)) maxId
                          ]
@@ -60,8 +68,8 @@ intToByteString = BS8.pack . show
 getUserTimeline :: (MonadThrow m, MonadIO m) => BS.ByteString -> OAuth -> Credential -> Manager -> Producer Status m ()
 getUserTimeline screenName oauth credential manager = getUserTimeline' screenName Nothing oauth credential manager
 
-getUserTimeline' :: 
-  (MonadThrow m, MonadIO m) => 
+getUserTimeline' ::
+  (MonadThrow m, MonadIO m) =>
   BS.ByteString ->
   Maybe BS.ByteString ->
   OAuth ->
@@ -87,8 +95,8 @@ getMaxId statuses = fmap (BS8.pack . show . statusId) (lastMay statuses)
 -- Parse into Status objects, throwing a ParseException if the string
 -- cannot be parsed.
 parseStatuses :: MonadThrow m => BL.ByteString -> m [Status]
-parseStatuses str = 
-  maybe 
+parseStatuses str =
+  maybe
     (throwM (ParseException ("Failed to parse: " <> (BL.unpack str))))
     return
     (decode str :: Maybe [Status])
@@ -108,29 +116,29 @@ identityPipe :: Monad m => Pipe a a m ()
 identityPipe = forever $ await >>= yield
 
 data Config = Config { configConsumerSecret :: String
-                     , configConsumerKey :: String
-                     , configToken :: String
-                     , configTokenSecret :: String
-                     , configScreenName :: String
-                     , configStatusCount :: Maybe Int
-                     , configMaxId :: Maybe String
+                     , configConsumerKey    :: String
+                     , configToken          :: String
+                     , configTokenSecret    :: String
+                     , configScreenName     :: String
+                     , configStatusCount    :: Maybe Int
+                     , configMaxId          :: Maybe String
                      } deriving (Show)
 
 configParserInfo :: ParserInfo Config
 configParserInfo = info (helper <*> configParser) (fullDesc <> progDesc "A utility for downloading a user's tweets.")
 
 configParser :: Parser Config
-configParser = Config 
+configParser = Config
   <$> strOption (long "csecret" <> help "Consumer secret.")
   <*> strOption (long "ckey" <> help "Consumer key.")
   <*> strOption (long "atoken" <> help "Access token.")
   <*> strOption (long "asecret" <> help "Access token secret.")
   <*> strOption (long "screenname" <> help "Screen name of account to download from.")
-  <*> (optional (option auto 
-        (long "count" 
+  <*> (optional (option auto
+        (long "count"
         <> help "Number of tweets to download. If omitted, as many tweets as the API allows will be downloaded."
         )))
-  <*> (optional (strOption 
+  <*> (optional (strOption
         (long "id"
         <> help "Tweet ID to start with (inclusive). If omitted, download will begin with most recent tweet."
         )))
