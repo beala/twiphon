a83d81c3c45c0b7ad29ae5008b66be6007e8535b
Alexander Beal <abeal@twitter.com>
2016-03-20 14:08:44 -0600

    Add more options. Remove dep on twitter-types.

--- README.md
+++ README.md
@@ -16,13 +16,15 @@ stack install
 Before using the program you must generate a consumer key, a consumer secret, an access token, and an access token secret. To generate these, first register an app with read permission at [https://apps.twitter.com/](https://apps.twitter.com/). Then go to the "Keys and Access Tokens" tab and generate the access tokens. Each of these are passed in on the command line. For example, to scrape my 1000 most recent tweets:
 
 ```
-twiphon --screenname beala --count 1000 --ckey my_consumer_key --csecret my_consumer_secret --atoken my_access_token --asecret my_access_token_secret
+  twiphon --secret consumer_secret --key consumer_key \
+    --token access_token --token_secret access_token_secret \
+    --screenname beala --count 1000
 ```
 
 The tweets will be written to stdout as newline separated JSON objects.
 
-See `twiphon --help` for additional information.
+See `twiphon --help` for additional options.
 
 ## Rate Limiting
 
-This program fails ungracefully when rate limited (the program is killed and an exception is printed to stderr). If you keep running it after getting rate limited you may find yourself blacklisted. Currently the API only allows a user's 3,200 most recent tweets to be downloaded. This tool uses the [GET statuses/user_timeline](https://dev.twitter.com/rest/reference/get/statuses/user_timeline) endpoint. Rate limiting information here: [https://dev.twitter.com/rest/public/rate-limits](https://dev.twitter.com/rest/public/rate-limits).
+This program quits when rate limited. If you keep running it after getting rate limited you may find yourself blacklisted. Currently the API only allows a user's 3,200 most recent tweets to be downloaded. This tool uses the [GET statuses/user_timeline](https://dev.twitter.com/rest/reference/get/statuses/user_timeline) endpoint. Rate limiting information here: [https://dev.twitter.com/rest/public/rate-limits](https://dev.twitter.com/rest/public/rate-limits).
--- app/Main.hs
+++ app/Main.hs
@@ -2,47 +2,91 @@
 
 module Main where
 
-import           Control.Exception.Base     (Exception)
+import           Control.Exception.Base     (Exception, catch)
 import           Control.Monad              (forever)
 import           Control.Monad.Catch        (MonadThrow, throwM)
 import           Control.Monad.IO.Class
-import           Data.Aeson                 (decode, encode)
+import           Data.Aeson                 (Value (..), encode)
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Char8      as BS8
 import qualified Data.ByteString.Lazy.Char8 as BL
-import           Data.Foldable              (traverse_)
-import           Data.Maybe                 (catMaybes)
+import           Data.Foldable              (find, traverse_)
 import           Data.Monoid                ((<>))
-import           Network.HTTP.Client        (Manager, Request, httpLbs,
+import qualified Data.Text.Encoding         as T
+import           Lens.Micro                 (toListOf, (^?), _last)
+import           Lens.Micro.Aeson           (key, values, _String)
+import           Network.HTTP.Client        (HttpException (..), Manager,
+                                             Request, Response, httpLbs,
                                              newManager, parseUrl, responseBody,
                                              setQueryString)
 import           Network.HTTP.Client.TLS    (tlsManagerSettings)
+import           Network.HTTP.Types.Header  (Header, HeaderName)
+import           Network.HTTP.Types.Status  (Status (..))
 import           Options.Applicative        (Parser, ParserInfo, auto,
                                              execParser, fullDesc, help, helper,
                                              info, long, option, optional,
-                                             progDesc, strOption, value)
+                                             progDesc, strOption, switch, value)
 import           Pipes
 import qualified Pipes.Prelude              as P
-import           Safe                       (lastMay)
+import           System.Exit                (exitFailure)
+import           System.IO                  (hPutStrLn, stderr)
 import           Web.Authenticate.OAuth     (Credential (..), OAuth, def,
                                              oauthConsumerKey,
                                              oauthConsumerSecret,
                                              oauthServerName, signOAuth)
-import           Web.Twitter.Types          (Status (..))
 
 main :: IO ()
 main = do
   config <- execParser configParserInfo
+
   let oauth = twitterOAuth config
   let credential = twitterCredential config
   let maxId = BS8.pack <$> (configMaxId config)
-  let screenName = BS8.pack $ configScreenName config
   let statusCount = configStatusCount config
+  identifier <- pickScreennameUserId (configScreenName config) (configUserId config)
+  let params = trimUserParam `unlessMonoid` configTrimUser config
+            <> excludeRepliesParam `unlessMonoid` configExcludeReplies config
+            <> contributorDetailsParam `unlessMonoid` configContribDetails config
+            <> excludeRtsParam `unlessMonoid` configExcludeRts config
+            <> countParam (configBatchSize config)
+            <> identifier
+
   manager <- newManager tlsManagerSettings
-  runEffect $ getUserTimeline screenName maxId oauth credential manager >->
-              P.take statusCount >->
-              statusToJson >->
-              printByteString
+  let pipeline = getUserTimeline maxId params oauth credential manager >->
+                 P.take statusCount >->
+                 statusToJson >->
+                 printByteString
+  (runEffect pipeline) `catch` (\e -> handleExceptions e >> exitFailure)
+
+putStrLnErr :: String -> IO ()
+putStrLnErr = hPutStrLn stderr
+
+findHeader :: HeaderName -> [Header] -> Maybe BS.ByteString
+findHeader headerName headers = fmap snd (find (\(name, _) -> name == headerName) headers)
+
+pickScreennameUserId :: MonadThrow m => Maybe String -> Maybe String -> m QueryParams
+pickScreennameUserId (Just screenname) _ = return [("screen_name", Just (BS8.pack screenname))]
+pickScreennameUserId _ (Just userId) = return [("user_id", Just (BS8.pack userId))]
+pickScreennameUserId _ _ = throwM $ ParamException "Must provide either --screenname or --userid."
+
+data ParamException = ParamException String deriving Show
+instance Exception ParamException
+
+handleExceptions :: HttpException -> IO ()
+handleExceptions (StatusCodeException (Status 429 _) headers _) =
+  let totalPermits = findHeader "x-rate-limit-limit" headers
+      remainingPermits = findHeader "x-rate-limit-remaining" headers
+      resetTimeStamp = findHeader "x-rate-limit-reset" headers in
+  putStrLnErr
+    ( "ERROR: Received 429 Over Rate Limit response."
+      <> "\n\tTotal permits: "
+      <> (show totalPermits)
+      <> "\n\tRemaining permits: "
+      <> (show remainingPermits)
+      <> "\n\tUnix timestamp (UTC) when permits will be reset: "
+      <> (show resetTimeStamp))
+handleExceptions e =
+  putStrLnErr ("ERROR: Received non-2XX response. Response dump: " <> show e)
 
 twitterOAuth :: Config -> OAuth
 twitterOAuth config = def
@@ -54,68 +98,105 @@ twitterOAuth config = def
 twitterCredential :: Config -> Credential
 twitterCredential config = Credential [(BS8.pack $ configToken config, BS8.pack $ configTokenSecret config)]
 
-userTimelineReq :: MonadThrow m => BS.ByteString -> Maybe BS.ByteString -> m Request
-userTimelineReq screenName maxId = do
+userTimelineReq :: MonadThrow m => QueryParams -> m Request
+userTimelineReq params = do
   initReq <- parseUrl "https://api.twitter.com/1.1/statuses/user_timeline.json"
-  let params = catMaybes [ Just ("screen_name", Just screenName)
-                         , fmap (\x -> ("max_id", Just x)) maxId
-                         ]
   return $ setQueryString params initReq
 
-intToByteString :: Int -> BS.ByteString
-intToByteString = BS8.pack . show
-
-getUserTimeline ::
+execUserTimelineReq ::
   (MonadThrow m, MonadIO m) =>
-  BS.ByteString ->
-  Maybe BS.ByteString ->
+  QueryParams ->
   OAuth ->
   Credential ->
   Manager ->
-  Producer Status m ()
-getUserTimeline screenName maxId oauth credential manager = do
-    tlReq       <- lift $ userTimelineReq screenName maxId
-    signedTlReq <- liftIO $ signOAuth oauth credential tlReq
-    response    <- liftIO $ httpLbs signedTlReq manager
-    statuses    <- lift $ parseStatuses (responseBody response)
+  m (Response BL.ByteString)
+execUserTimelineReq params oauth credential manager = do
+  tlReq        <- userTimelineReq params
+  signedTlReq  <- liftIO $ signOAuth oauth credential tlReq
+  liftIO $ httpLbs signedTlReq manager
+
+type QueryParams = [(BS.ByteString, Maybe BS.ByteString)]
+
+maxIdParam :: BS.ByteString -> QueryParams
+maxIdParam i = [("max_id", Just i)]
+
+screenNameParam :: BS.ByteString -> QueryParams
+screenNameParam sn = [("screen_name", Just sn)]
+
+trimUserParam :: QueryParams
+trimUserParam = [("trim_user", Just "true")]
+
+excludeRepliesParam :: QueryParams
+excludeRepliesParam = [("exclude_replies", Just "true")]
+
+contributorDetailsParam :: QueryParams
+contributorDetailsParam = [("contributor_details", Just "true")]
+
+excludeRtsParam :: QueryParams
+excludeRtsParam = [("include_rts", Just "false")]
+
+countParam :: Int -> QueryParams
+countParam c = [("count", Just (BS8.pack (show c)))]
+
+unlessMonoid :: Monoid m => m -> Bool -> m
+unlessMonoid _ False = mempty
+unlessMonoid m True = m
+
+getUserTimeline ::
+  (MonadThrow m, MonadIO m) =>
+  Maybe BS.ByteString ->
+  QueryParams ->
+  OAuth ->
+  Credential ->
+  Manager ->
+  Producer Value m ()
+getUserTimeline maybeMaxId extraParams oauth credential manager = do
+    let params   = (maybe mempty maxIdParam maybeMaxId) <> extraParams
+    response     <- lift $ execUserTimelineReq params oauth credential manager
+    let statuses = parseStatuses (BL.toStrict (responseBody response))
+
     traverse_ yield statuses
-    case getMaxId statuses of
+
+    case nextPage statuses maybeMaxId of
       Just newMaxId ->
-        -- First tweet will be a duplicate of the last tweet in the previous request, so it is dropped.
-        getUserTimeline screenName (Just newMaxId) oauth credential manager >-> P.drop 1
+        getUserTimeline (Just newMaxId) extraParams oauth credential manager >-> P.drop 1
       Nothing -> return ()
 
--- Return the ID of the last tweet in the list if there is one.
-getMaxId :: [Status] -> Maybe BS.ByteString
-getMaxId statuses = fmap (BS8.pack . show . statusId) (lastMay statuses)
+-- Decide if the next page should be retreived. Return `Nothing` if it shouldn't.
+-- Return the next maxId if it should.
+nextPage :: [Value] -> Maybe BS.ByteString -> Maybe BS.ByteString
+nextPage [] _ = Nothing
+nextPage statuses lastMaxId =
+  if getMaxId statuses == lastMaxId
+    then Nothing
+    else getMaxId statuses
 
--- Parse into Status objects, throwing a ParseException if the string
--- cannot be parsed.
-parseStatuses :: MonadThrow m => BL.ByteString -> m [Status]
-parseStatuses str =
-  maybe
-    (throwM (ParseException ("Failed to parse: " <> (BL.unpack str))))
-    return
-    (decode str :: Maybe [Status])
+getMaxId :: [Value] -> Maybe BS.ByteString
+getMaxId statuses = fmap T.encodeUtf8 (statuses ^? _last . (key "id_str") . _String)
 
-data ParseException = ParseException String deriving (Show)
-
-instance Exception ParseException
+parseStatuses :: BS.ByteString -> [Value]
+parseStatuses = toListOf values
 
 -- Serialize status back into JSON.
-statusToJson :: MonadThrow m => Pipe Status BS.ByteString m ()
+statusToJson :: MonadThrow m => Pipe Value BS.ByteString m ()
 statusToJson = forever $ await >>= yield . BL.toStrict . encode
 
 printByteString :: MonadIO m => Consumer BS.ByteString m ()
 printByteString = forever $ await >>= (liftIO . BS8.putStrLn)
 
-data Config = Config { configConsumerSecret :: String
-                     , configConsumerKey    :: String
+data Config = Config { configConsumerKey    :: String
+                     , configConsumerSecret :: String
                      , configToken          :: String
                      , configTokenSecret    :: String
-                     , configScreenName     :: String
+                     , configScreenName     :: Maybe String
+                     , configUserId         :: Maybe String
                      , configStatusCount    :: Int
                      , configMaxId          :: Maybe String
+                     , configTrimUser       :: Bool
+                     , configExcludeRts     :: Bool
+                     , configContribDetails :: Bool
+                     , configExcludeReplies :: Bool
+                     , configBatchSize      :: Int
                      } deriving (Show)
 
 configParserInfo :: ParserInfo Config
@@ -123,17 +204,26 @@ configParserInfo = info (helper <*> configParser) (fullDesc <> progDesc "A utili
 
 configParser :: Parser Config
 configParser = Config
-  <$> strOption (long "csecret" <> help "Consumer secret.")
-  <*> strOption (long "ckey" <> help "Consumer key.")
-  <*> strOption (long "atoken" <> help "Access token.")
-  <*> strOption (long "asecret" <> help "Access token secret.")
-  <*> strOption (long "screenname" <> help "Screen name of account to download from.")
+  <$> strOption (long "key" <> help "Consumer key.")
+  <*> strOption (long "secret" <> help "Consumer secret.")
+  <*> strOption (long "token" <> help "Access token.")
+  <*> strOption (long "token_secret" <> help "Access token secret.")
+  <*> optional (strOption (long "screenname" <> help "Screen name of account to download from. Do not use in conjunction with userid."))
+  <*> optional (strOption (long "userid" <> help "User ID of account to download from. Do not use in conjunction with screenname."))
   <*> option auto
         (long "count"
         <> help "Number of tweets to download. If omitted, as many tweets as the API allows (3200) will be downloaded."
         <> value 3200
         )
   <*> (optional (strOption
-        (long "id"
-        <> help "Tweet ID to start with (inclusive). If omitted, download will begin with most recent tweet."
+        (long "max_id"
+        <> help ("Set an upper bound on the tweet ID (inclusive). Don't fetch tweets newer than this ID. "
+                <> "If omitted, download will begin with most recent tweet."
+                )
         )))
+  <*> switch (long "trim_user" <> help "Include only the author's numerical ID. Trim other details.")
+  <*> switch (long "exclude_rts" <> help "Exclude native retweets from results.")
+  <*> switch (long "contrib_details" <> help "Include additional contributor info, rather than just the user's ID.")
+  <*> switch (long "exclude_replies" <> help "Exclude replies from results.")
+  <*> option auto
+        (long "batch_size" <> value 200 <> help "Status to fetch per request. Must be between 2 and 200 inclusive. Defaults to 200.")
